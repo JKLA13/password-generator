16ae0a83c0847522d45295829efe4b75ed881aa9
Jacob Adamson <jakeladamson@gmail.com>
2021-07-30 15:29:07 -0600

    added if validator and alert method

--- script.js
+++ script.js
@@ -1,95 +1,102 @@
 // global variables with arrays of characters to use
 
 var lowerCaseArr = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 var upperCaseArr = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-var numericArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var specialCharArr = [
-  '?',
-  '@',
-  '%',
-  '+',
-  '/',
+  "?",
+  "@",
+  "%",
+  "+",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
+  "!",
+  "#",
+  "$",
+  "^",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // function to grab user input from prompts
 
 function userPrompts() {
-// prompt to get length of password
-  var pwLength = parseInt(prompt("How long do you want your password to be?"))
+  // prompt to get length of password
+  var pwLength = parseInt(prompt("How long do you want your password to be?"));
+
+  //if statement to validate pw length
+
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Your password needs to be in between 8 and 128 characters.");
+    return null;
+  }
 }
 
 // Assignment Code
@@ -101,7 +108,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
